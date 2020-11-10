440c028ba31daeda746da30a2953cc8a38aeb3bc
ApoorvMehrotra0706 <apoorv.mehrotra@sjsu.edu>
2020-11-09 22:51:38 -0800

    Prcoessor class

--- IndividualProject/src/main/java/cmpe202/IndividualProject/Processor.java
+++ IndividualProject/src/main/java/cmpe202/IndividualProject/Processor.java
@@ -5,25 +5,10 @@ import java.util.List;
 public class Processor {
     
     public void processCard(String inputFile, String outputFile) {
-        // String inputFile = "C:\\202\\Sample.json";
 		FileFactory fileFactory = new FileFactory();
 		FileFormat f = fileFactory.checkFileFormat(inputFile);
 		List<CreditCard> list = f.getCreditCardDetails(inputFile);
 		System.out.println(list);
 		f.writeFile(list, outputFile);
-
-		// String inputFile = "C:\\202\\Sample.xml";
-		// FileFactory fileFactory = new FileFactory();
-		// FileFormat f = fileFactory.checkFileFormat(inputFile);
-		// List<CreditCard> list = f.getCreditCardDetails(inputFile);
-		// System.out.println(list);
-		// f.writeFile(list, "C:\\202\\XMLOutput.xml");
-
-		// String inputFile = "C:\\202\\Sample.csv";
-		// FileFactory fileFactory = new FileFactory();
-		// FileFormat f = fileFactory.checkFileFormat(inputFile);
-		// List<CreditCard> list = f.getCreditCardDetails(inputFile);
-		// System.out.println(list);
-		// f.writeFile(list, "C:\\202\\CSVOutput.csv");
     }
 }
