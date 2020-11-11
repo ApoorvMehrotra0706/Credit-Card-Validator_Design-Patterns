b83475327c3cb27b37d0853db56d91848f7e087a
ApoorvMehrotra0706 <apoorv.mehrotra@sjsu.edu>
2020-11-11 13:26:45 -0800

    Part 1 Documentation

--- IndividualProject/src/main/java/cmpe202/IndividualProject/App.java
+++ IndividualProject/src/main/java/cmpe202/IndividualProject/App.java
@@ -1,12 +1,9 @@
 package cmpe202.IndividualProject;
 
-import java.util.Scanner;
-
 public class App 
 {
 	public static void main( String[] args )
     {
-		Scanner sc = new Scanner(System.in);
 		String inputFile = args[0];
 		String outputFile = args[1];
 		Processor processor = new Processor();
